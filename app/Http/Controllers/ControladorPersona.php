a21c34168ed71e4a8fe10b4e8554fc61974d1353
Marcelo De Leon <marcelodeleon290200@gmail.com>
2024-07-20 15:22:31 -0300

    alta de persona

---
+++ app/Http/Controllers/ControladorPersona.php
@@ -0,0 +1,12 @@
+public function alta(Request $request)
+{
+    $request->validate([
+        'nombre' => 'required',
+        'apellido' => 'required',
+        'telefono' => 'required',
+        'id' => 'required',
+    ]);
+
+    return Persona::create($request->all());
+}
+
