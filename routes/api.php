dbf1d077ec0fc933f8b13669273d0b36ad03fc75
MarceloDL2 <marcelodeleon290200@gmail.com>
2024-07-20 20:21:02 -0300

    Agregado rutas api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('personas', PersonaController::class);
+Route::get('personas/buscar', [PersonaController::class, 'buscar']);
